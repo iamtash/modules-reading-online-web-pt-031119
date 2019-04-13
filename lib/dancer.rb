cfac47efa718b88d98a54282772cb7029c3ed3c9
Natasha Chernis <natashachernis@gmail.com>
2019-04-13 22:20:46 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,12 +3,12 @@
 require_relative './fancy_dance.rb'
 
 class Dancer
-  extend MetaDancing
-  include Dance
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
-  attr_accessor :name
-
-  def initialize(name)
-    @name = name
-  end
+  # attr_accessor :name
+  # 
+  # def initialize(name)
+  #   @name = name
+  # end
 end
